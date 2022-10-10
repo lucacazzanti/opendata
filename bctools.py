fbd565d976400d3700e84423e2d3bf89f2ca9308
Luca Cazzanti <luca@ieee.org>
2022-10-10 10:36:17 -0700

    adding files for completeness

---
+++ bctools.py
@@ -0,0 +1,28 @@
+
+#! ~/anaconda/bin/python
+# -*- coding: utf-8 -*-
+# @author=lucacazzanti
+
+__doc__ = """Utilities for the Blue Crow Analytics exercise."""
+
+__docformat__ = 'restructuredtext'
+
+#import string
+#import re
+#import os
+#import csv
+#import itertools
+#import itertools_recipes
+#from bs4 import BeautifulSoup as bs
+#import numpy as np
+#from unidecode import unidecode
+#import random
+
+import json
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    import doctest
+    doctest.testmod()
